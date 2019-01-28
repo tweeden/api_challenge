4d5768f18c6aa9fb272016b9336dfdeb6c5141ac
tweeden <tweeden36@gmail.com>
2019-01-28 10:31:30 -0600

    setup a review server with now

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ const inputValue = document.querySelector("#search");
 const searchButton = document.querySelector(".searchButton");
 const nameContainer = document.querySelector(".main__profile-name");
 const unContainer = document.querySelector(".main__profile-username");
+const followersContainer = document.querySelector(".main__profile-followers");
+const idContainer = document.querySelector(".main__profile-id");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
 
@@ -26,8 +28,12 @@ const fetchUsers = async (user) => {
 const showData = () => {
   fetchUsers(inputValue.value).then((res) =>{
     console.log(res);
-
     nameContainer.innerHTML = `Name: <span class="main__profile-value">${res.data.name}</span>`;
+    unContainer.innerHTML = `Username: <span class="main__profile-value">${res.data.login}</span>`;
+    followersContainer.innerHTML = `Followers: <span class="main__profile-value">${res.data.followers}</span>`;
+    idContainer.innerHTML = `Id: <span class="main__profile-value">${res.data.id}</span>`;
+    reposContainer.innerHTML = `Repos: <span class="main__profile-value">${res.data.public_repos}</span>`;
+    urlContainer.innerHTML = `URL: <span class="main__profile-value">${res.data.url}</span>`;
   });
 };
 
